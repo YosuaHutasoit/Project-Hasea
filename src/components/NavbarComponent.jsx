bfdfbe4d49e3e92546c7289e395298791716186f
Yosshhh <yosuahutasoit082@gmail.com>
2024-12-09 15:33:45 +0700

    add hero banner

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,36 +1,54 @@
+import {useState, useEffect} from "react"
 import { Navbar, Container, Nav } from "react-bootstrap";
 
 import { navLinks } from "../data/index";
 import { NavLink } from "react-router-dom";
 
-function NavbarComponent() {
+const NavbarComponent = () => {
+
+  const [changeColor, setChangeColor] = useState(false);
+
+  const changeBackgroundColor = () => {
+    if (window.scrollY > 10) {
+      setChangeColor(true);
+    } else {
+      setChangeColor(false);
+    }
+  };
+
+  useEffect(() => {
+    changeBackgroundColor();
+    window.addEventListener('scroll', changeBackgroundColor);
+  }, []);
+
   return (
     <div>
-      <Navbar expand="lg">
+      <Navbar expand="lg" className= {changeColor ? "color-active" : "" }>
         <Container>
           <Navbar.Brand href="#home" className="d-flex align-items-center">
             <img
-              src="/Hasea.png" // Ganti path sesuai dengan lokasi file logo
+              src="/Hasea.png"
               alt="Logo"
               width="50"
               height="50"
-              className="me-2" // Memberi margin di sebelah kanan logo
+              className="me-2" 
             />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mx-auto">
+            <Nav className="mx-auto text-center">
               {navLinks.map((link) => {
                 return (
                   <div className="nav-link" key={link.id}>
                     <NavLink
                       to={link.path}
-                      className={({ isActive }) =>
-                        isActive
-                          ? "text-decoration-none"
-                          : "text-dark text-decoration-none"
-                      }
-                    >
+                      className={({ isActive, isPending, isTransitioning }) =>
+                        [
+                          isPending ? "pending" : "",
+                          isActive ? "active" : "",
+                          isTransitioning ? "transitioning" : "",
+                        ].join(" ")
+                      } end >
                       {link.text}
                     </NavLink>
                   </div>
@@ -38,8 +56,8 @@ function NavbarComponent() {
               })}
             </Nav>
 
-            <div>
-              <button>Penawaran</button>
+            <div className="text-center">
+              <button className="btn-penawaran" >Penawaran</button>
             </div>
           </Navbar.Collapse>
         </Container>
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,21 @@
-function LandingPage() {
+import {Container, Row, Col} from "react-bootstrap"
+import HeroBanner from '../assets/img/landingpage/Hero-Banner.png'
+
+const LandingPage = () => {
   return (
     <div className="homepage">
       <header className="w-100 min-vh-100">
-
+      <Container>
+        <Row>
+          <Col>
+          <h1>Test</h1>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic eligendi error non animi iure, praesentium blanditiis excepturi a, ratione eum dicta ipsam nobis saepe dolorum eveniet similique, deserunt iste fugiat!</p>
+          </Col>
+          <Col> <img src={HeroBanner} alt="hero-img" /></Col>
+        </Row>
+      </Container>
       </header>
+      <div className="kelas w-100 min-vh-100"></div>
     </div>
   )
 }
