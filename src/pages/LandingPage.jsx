fea32ca4a73a460c3a74e483b8decfd8690996d8
Yosshhh <yosuahutasoit082@gmail.com>
2024-12-11 10:26:50 +0700

    changes hero-section

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,57 +1,67 @@
-import {Container, Row, Col} from "react-bootstrap"
-import HeroBanner from '../assets/img/landingpage/Hero-Banner.png'
-import HeroBanner1 from '../assets/img/landingpage/Hero-Banner1.png'
+import { Container, Row, Col, Button } from "react-bootstrap";
+import HeroBanner from "../assets/img/landingpage/Hero-Banner.png";
 
 // import {} from '../data/index'
-import { useNavigate } from "react-router-dom"
 
 const LandingPage = () => {
-
-  let navigate = useNavigate()
-
   return (
-  <div>
-    <div className="homepage">
-      <header className="hero-section" style={{ backgroundImage: `url(${HeroBanner})` }}>
-        <div className="overlay">
-          <Container className="text-light hero-content">
-            <Row className="align-items-center">
-              <Col md={12}>
-                <h1 className="hero-title">PT HASEA BEDIVAN ANUGERAH</h1>
-                <p className="hero-subtitle">
-                  Solusi Penyedia Besi, Baja, & Material Terbaik Untuk Semua Proyek Anda!
+    <div>
+      <div className="homepage">
+        <header
+          className="hero-section"
+          style={{ backgroundImage: `url(${HeroBanner})` }}
+        >
+          <div className="overlay">
+            <Container className="text-light hero-content">
+              <Row className="align-items-center">
+                <Col md={12}>
+                  <h1 className="hero-title">PT HASEA BEDIVAN ANUGERAH</h1>
+                  <p className="hero-subtitle">
+                    Solusi Penyedia Besi, Baja, & Material Terbaik Untuk Semua
+                    Proyek Anda!
+                  </p>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        </header>
+      </div>
+      <div className="about-us">
+        <div className="kelas w-100 min-vh-100">
+          <Container className="overflow-hidden">
+            <Row className="gy-3 gy-lg-0 align-items-lg-center justify-content-lg-between">
+              <Col xs={12} lg={6} className="order-1 order-lg-0">
+                <h2 className="display-3 fw-bold mb-3">Art of Design</h2>
+                <p className="fs-4 mb-4">
+                  Powerful, extensible, and feature-packed frontend toolkit.
+                  Build and customize with Sass, utilize prebuilt grid system
+                  and components, and bring projects to life with powerful
+                  JavaScript plugins.
                 </p>
+                <div className="d-grid gap-2 d-sm-flex">
+                  <Button
+                    variant="primary"
+                    size="lg"
+                    className="bsb-btn-xl rounded-pill"
+                    as="a"
+                    href="/aboutus"
+                  >
+                    Free Consultation
+                  </Button>
+                </div>
               </Col>
             </Row>
           </Container>
         </div>
-      </header>
       </div>
-    <div className="about-us">
-    
-      {/* <div className="kelas w-100 min-vh-100">
-        <Container>
-          <Row>
-            <Col>
-            <img src={HeroBanner1} alt="hero-img" />
-            </Col>
-            <Col>
-            <h1>PT HASEA BEDIIVAN ANUGERAH</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates nostrum harum sed a quisquam illo reprehenderit doloribus inventore? Ratione rem cum earum eligendi quos error harum! Ex quas deserunt nemo culpa, modi impedit quam illum vero in. Quae neque natus assumenda sint unde sed ut ipsum laudantium voluptatibus. Nam, delectus.</p>
-            <button onClick={() => navigate('/aboutus')}>read more..</button>
-            </Col>
-          </Row>
-        </Container>
-      </div> */}
+      <div className="products">
+        <div className="kelas w-100 min-vh-100"></div>
+      </div>
+      <div className="contactme">
+        <div className="kelas w-100 min-vh-100"></div>
+      </div>
     </div>
-    <div className="products">
-      <div className="kelas w-100 min-vh-100"></div>
-    </div>
-    <div className="contactme">
-      <div className="kelas w-100 min-vh-100"></div>
-    </div>
-  </div>
   );
 };
 
-export default LandingPage
+export default LandingPage;
