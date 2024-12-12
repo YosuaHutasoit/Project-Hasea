69994561685278abc28a08648aa142dfaa59f49c
Yosshhh <yosuahutasoit082@gmail.com>
2024-12-12 22:09:52 +0700

    add hero section

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,6 @@
 import { Container, Row, Col, Button } from "react-bootstrap";
 import HeroBanner from "../assets/img/landingpage/Hero-Banner.png";
+import HeroBanner1 from "../assets/img/landingpage/Hero-Banner1.png";
 
 // import {} from '../data/index'
 
@@ -26,33 +27,46 @@ const LandingPage = () => {
           </div>
         </header>
       </div>
-      <div className="about-us">
-        <div className="kelas w-100 min-vh-100">
-          <Container className="overflow-hidden">
-            <Row className="gy-3 gy-lg-0 align-items-lg-center justify-content-lg-between">
-              <Col xs={12} lg={6} className="order-1 order-lg-0">
-                <h2 className="display-3 fw-bold mb-3">Art of Design</h2>
-                <p className="fs-4 mb-4">
-                  Powerful, extensible, and feature-packed frontend toolkit.
-                  Build and customize with Sass, utilize prebuilt grid system
-                  and components, and bring projects to life with powerful
-                  JavaScript plugins.
-                </p>
-                <div className="d-grid gap-2 d-sm-flex">
-                  <Button
-                    variant="primary"
-                    size="lg"
-                    className="bsb-btn-xl rounded-pill"
-                    as="a"
-                    href="/aboutus"
-                  >
-                    Free Consultation
-                  </Button>
-                </div>
-              </Col>
-            </Row>
-          </Container>
-        </div>
+      <div className="about-us w-100 min-vh-100">
+        <Container className="overflow-hidden">
+          <Row className="gy-3 gy-lg-0 align-items-lg-center justify-content-lg-between">
+            <Col xs={12} lg={5} className="text-center">
+              <img
+                className="img-fluid"
+                loading="lazy"
+                src={HeroBanner1}
+                alt="Art of Design"
+                style={{}}
+              />
+            </Col>
+            <Col xs={12} lg={6} className="order-1 order-lg-0">
+              <h2 className="display-7 fw-bold mb-3" style={{ color: '#FF5733' }}>
+                PT HASEA BEDIIVAN ANUGERAH
+              </h2>
+              <p className="fs-10 mb-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <div className="d-grid gap-2 d-sm-flex">
+                <Button
+                  variant="primary"
+                  size="lg"
+                  className="bsb-btn-xl"
+                  as="a"
+                  href="/aboutus"
+                  style={{ borderRadius: "12px",  fontSize: "15px" }}
+                >
+                  Read More...
+                </Button>
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </div>
       <div className="products">
         <div className="kelas w-100 min-vh-100"></div>
